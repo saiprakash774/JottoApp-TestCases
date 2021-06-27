865aad0e499669d9aa1430ec51a98d56020f9929
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2021-06-28 00:52:26 +0530

    Added Bootstrap Styling

--- src/GuessedWord.js
+++ src/GuessedWord.js
@@ -19,8 +19,8 @@ function GuessedWord(props) {
         contents=(
         <div data-test='guessed-words'>
             <h3>Guessed Words</h3>
-            <table>
-                <thead>
+            <table className="table table-sm">
+                <thead className="thead-light">
                     <tr><th>Guess</th><th>Matching letters</th></tr>
                 </thead>
                 <tbody>

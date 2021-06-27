52871ed36a732464bd5d1bca4a7d53c1c1afae07
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2021-06-28 01:45:42 +0530

    useState to capture input change event

--- src/Input.js
+++ src/Input.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropType from 'prop-types';
-
 function Input({secretWord}) {
+    const [currentGuess,setCurrentGuess]= React.useState("");
     return (
         <div data-test='component-input'>
-            
+            <form className="form-inline">
+                <input data-test='input-box' className='mb-2 mx-ms-3' type="text" 
+                placeholder="Enter Guess" value={currentGuess} onChange={(event)=>setCurrentGuess(event.target.value)}/>
+                <button data-test='submit-button' className='btn btn-primary mb-2'>Submit</button>
+            </form>
         </div>
     )
 }
